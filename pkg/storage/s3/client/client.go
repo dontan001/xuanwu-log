49883ba6ad973745662d624d2d0cdb9bcb06b5aa
dongge.tan <dontan001@hotmail.com>
2022-09-07 16:31:40 +0800

    s3

--- pkg/storage/s3/client/client.go
+++ pkg/storage/s3/client/client.go
@@ -1,1 +1,144 @@
 package client
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3iface"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager/s3manageriface"
+
+	"github.com/cortexproject/cortex/pkg/util/flagext"
+	"github.com/pkg/errors"
+)
+
+type S3Config struct {
+	S3ForcePathStyle bool
+	Insecure         bool
+	Level            aws.LogLevelType
+
+	BucketNames string
+	Endpoint    string
+	Region      string
+
+	AccessKeyID     string
+	SecretAccessKey flagext.Secret
+}
+
+type S3ObjectClient struct {
+	cfg          S3Config
+	S3           s3iface.S3API
+	S3Uploader   s3manageriface.UploaderAPI
+	S3Downloader s3manageriface.DownloaderAPI
+}
+
+func NewS3ObjectClient(cfg S3Config) (*S3ObjectClient, error) {
+	config, err := generateAwsConfig(cfg)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to generate aws config")
+	}
+
+	s3Client, err := buildS3Client(config)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to build s3 client")
+	}
+
+	s3Uploader, err := buildS3Uploader(config)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to build s3 uploader")
+	}
+
+	s3Downloader, err := buildS3Downloader(config)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to build s3 downloader")
+	}
+
+	client := S3ObjectClient{
+		cfg:          cfg,
+		S3:           s3Client,
+		S3Uploader:   s3Uploader,
+		S3Downloader: s3Downloader,
+	}
+	return &client, nil
+}
+
+func buildS3Client(config *aws.Config) (*s3.S3, error) {
+	sess, err := session.NewSession(config)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create new s3 session")
+	}
+
+	s3Client := s3.New(sess)
+	return s3Client, nil
+}
+
+func buildS3Uploader(config *aws.Config) (*s3manager.Uploader, error) {
+	sess, err := session.NewSession(config)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create new s3 session")
+	}
+
+	s3Uploader := s3manager.NewUploader(sess)
+	return s3Uploader, nil
+}
+
+func buildS3Downloader(config *aws.Config) (*s3manager.Downloader, error) {
+	sess, err := session.NewSession(config)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create new s3 session")
+	}
+
+	s3Downloader := s3manager.NewDownloader(sess)
+	return s3Downloader, nil
+}
+
+func generateAwsConfig(cfg S3Config) (*aws.Config, error) {
+	config := aws.NewConfig()
+
+	config = config.WithMaxRetries(3)
+	config = config.WithS3ForcePathStyle(cfg.S3ForcePathStyle)
+
+	if cfg.Insecure {
+		config = config.WithDisableSSL(true)
+	}
+
+	if cfg.Level != 0 {
+		config = config.WithLogLevel(cfg.Level)
+	}
+
+	if cfg.Endpoint != "" {
+		config = config.WithEndpoint(cfg.Endpoint)
+	}
+
+	if cfg.Region != "" {
+		config = config.WithRegion(cfg.Region)
+	}
+
+	if cfg.AccessKeyID != "" && cfg.SecretAccessKey.String() == "" ||
+		cfg.AccessKeyID == "" && cfg.SecretAccessKey.String() != "" {
+		return nil, errors.New("must supply both an Access Key ID and Secret Access Key or neither")
+	}
+
+	if cfg.AccessKeyID != "" && cfg.SecretAccessKey.String() != "" {
+		creds := credentials.NewStaticCredentials(cfg.AccessKeyID, cfg.SecretAccessKey.String(), "")
+		config = config.WithCredentials(creds)
+	}
+
+	transport := http.RoundTripper(&http.Transport{
+		Proxy:                 http.ProxyFromEnvironment,
+		MaxIdleConns:          200,
+		MaxIdleConnsPerHost:   200,
+		TLSHandshakeTimeout:   3 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+	})
+	httpClient := &http.Client{
+		Transport: transport,
+	}
+
+	config = config.WithHTTPClient(httpClient)
+	return config, nil
+}
---
+++ pkg/storage/s3/object.go
@@ -0,0 +1,160 @@
+package s3
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"math"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/endpoints"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+
+	"github.com/cortexproject/cortex/pkg/util/flagext"
+	"github.com/kyligence/xuanwu-log/pkg/storage/s3/client"
+)
+
+var (
+	bucket = "donggetest"
+
+	cfg = client.S3Config{
+		Level:           aws.LogDebugWithHTTPBody,
+		Insecure:        true,
+		Region:          endpoints.UsWest2RegionID,
+		AccessKeyID:     AccessKeyID,
+		SecretAccessKey: flagext.Secret{Value: SecretAccessKey},
+	}
+
+	objectClient, _ = client.NewS3ObjectClient(cfg)
+)
+
+func GetBuckets() error {
+	ctx := context.Background()
+
+	result, err := objectClient.S3.ListBucketsWithContext(ctx, nil)
+	if err != nil {
+		log.Printf("Unable to list buckets, %v", err)
+	}
+
+	for _, b := range result.Buckets {
+		fmt.Printf("* %s created on %s\n",
+			aws.StringValue(b.Name), aws.TimeValue(b.CreationDate))
+	}
+
+	return nil
+}
+
+func GetObjects() error {
+	ctx := context.Background()
+
+	objects := []string{}
+	err := objectClient.S3.ListObjectsPagesWithContext(ctx, &s3.ListObjectsInput{
+		Bucket: aws.String(bucket),
+	}, func(p *s3.ListObjectsOutput, lastPage bool) bool {
+		for _, o := range p.Contents {
+			objects = append(objects, aws.StringValue(o.Key))
+		}
+		return true // continue paging
+	})
+	if err != nil {
+		return fmt.Errorf("failed to list objects for bucket, %s, %v", bucket, err)
+	}
+
+	for idx, obj := range objects {
+		fmt.Printf("Objects in bucket: %d, %s \n", idx, obj)
+	}
+	return nil
+}
+
+func GetObject() error {
+	ctx := context.Background()
+
+	result, err := objectClient.S3.GetObjectWithContext(ctx, &s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String("index/loki_index_19240/loki-loki-distributed-ingester-0-1662344787333739953-1662348480.gz"),
+	})
+	if err != nil {
+		// Cast err to awserr.Error to handle specific error codes.
+		aerr, ok := err.(awserr.Error)
+		if ok && aerr.Code() == s3.ErrCodeNoSuchKey {
+			// Specific error code handling
+		}
+		return err
+	}
+
+	// Make sure to close the body when done with it for S3 GetObject APIs or
+	// will leak connections.
+	defer result.Body.Close()
+
+	fmt.Println("Object Size:", aws.Int64Value(result.ContentLength))
+	return nil
+}
+
+func HeadObject() error {
+	result, err := objectClient.S3.HeadObject(&s3.HeadObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String("index/loki_index_19240/loki-loki-distributed-ingester-0-1662344787333739953-1662348480.gz"),
+	})
+	if err != nil {
+		aerr, ok := err.(awserr.Error)
+		if ok && aerr.Code() == s3.ErrCodeNoSuchKey {
+			// Specific error code handling
+		}
+		return err
+	}
+
+	fmt.Println(result)
+	return nil
+}
+
+func PutObject(remotePath, srcFile string) error {
+	file, err := os.Open(srcFile)
+	if err != nil {
+		return fmt.Errorf("unable to open file %v", err)
+	}
+
+	defer file.Close()
+	info, err := file.Stat()
+	if err != nil {
+		return fmt.Errorf("failed to get size of file %s: %v", srcFile, err)
+	}
+
+	_, err = objectClient.S3Uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(remotePath),
+		Body:   file,
+	}, func(uploader *s3manager.Uploader) {
+		// set the part size as low as possible to avoid timeouts and aborts
+		var partSize int64 = s3manager.MinUploadPartSize
+		maxParts := math.Ceil(float64(info.Size() / partSize))
+
+		// 10000 parts is the limit for AWS S3. If the resulting number of parts would exceed that limit, increase the
+		// part size as much as needed but as little possible
+		if maxParts > s3manager.MaxUploadParts {
+			partSize = int64(math.Ceil(float64(info.Size()) / s3manager.MaxUploadParts))
+		}
+
+		uploader.Concurrency = s3manager.DefaultUploadConcurrency
+		uploader.LeavePartsOnError = false
+		uploader.PartSize = partSize
+	})
+	if err != nil {
+		return fmt.Errorf("unable to upload %s to %s, %v", srcFile, bucket, err)
+	}
+
+	log.Printf("Successfully uploaded %q to %q\n", srcFile, bucket)
+	return nil
+}
+
+func DelObject(remotePath string) error {
+	input := &s3.DeleteObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(remotePath),
+	}
+
+	_, err := objectClient.S3.DeleteObject(input)
+	return err
+}
