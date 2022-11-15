949aed88c1d994617c0158a969f3f0607e31a0fe
dongge.tan <dontan001@hotmail.com>
2022-11-15 11:14:16 +0800

    query unescape test

--- pkg/util/util_test.go
+++ pkg/util/util_test.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"net/url"
 	"testing"
 )
 
@@ -15,4 +16,20 @@ func TestDivMod(t *testing.T) {
 
 	d, m = DivMod(7, 6)
 	t.Logf("%d %d", d, m)
+}
+
+func TestQueryUnescape(t *testing.T) {
+	raw := "%7Bapp=%22yl-common-booter%22,namespace=%22yinglong-dev%22,job=%22fluent-bit%22%7D"
+	unescaped, err := url.QueryUnescape(raw)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+	t.Logf("%v", unescaped)
+
+	raw = "{app=\"yl-common-booter\",namespace=\"yinglong-dev\",job=\"fluent-bit\"}"
+	unescaped, err = url.QueryUnescape(raw)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+	t.Logf("%v", unescaped)
 }
